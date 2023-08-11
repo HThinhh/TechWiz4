6a0250c28d0e3cdfc1f7f8cc153ab982e6197006
hp <hp@LAPTOP-DP4TP1GE>
2023-08-11 23:44:23 +0700

    ttt

--- PlantPlace/asset/js/homepage.js
+++ PlantPlace/asset/js/homepage.js
@@ -4,18 +4,19 @@ const search = () => {
 	const storeitems = document.getElementById("product-list")
 	const product = document.querySelectorAll(".product")
 	const productname = document.getElementsByTagName("p")
-
+	let d=0;
 	for(var i = 0; i < productname.length; i++){
 		let match = product[i].getElementsByTagName("p")[0];
 
 		if(match){
 			let textvalue = match.textContent || match.innerHTML
-
 			if(textvalue.toUpperCase().indexOf(searchbox) > -1){
 				product[i].style.display = "";
+				d++;
 			}else{
 				product[i].style.display = "none";
 			}
 		}
+		document.getElementById("sl").innerHTML=d;
 	}
  }
